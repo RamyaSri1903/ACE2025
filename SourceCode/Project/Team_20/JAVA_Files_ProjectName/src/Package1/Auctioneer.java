c376f06ee65b3a0bb873c78ea5055b43a87248be
RamyaSri1903 <161117753+RamyaSri1903@users.noreply.github.com>
2024-09-25 14:04:34 +0530

    Team_20

--- SourceCode/Project/Team_20/JAVA_Files_ProjectName/src/Package1/Auctioneer.java
+++
@@ -1,7 +0,0 @@
-public class Auctioneer extends User {
-    public Auctioneer(String username, String password, String email) {
-        super(username, password, email);
-    }
-    public void createAuction(Auction auction) {
-    }
-}
--- SourceCode/Project/Team_20/JAVA_Files_ProjectName/src/Package1/Bidder.java
+++
@@ -1,5 +0,0 @@
-public class Bidder extends User {
-    public Bidder(String username, String password, String email) {
-        super(username, password, email);
-    }
-}
